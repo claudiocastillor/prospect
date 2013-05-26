210578a4fd5011101287a1d4146d000775ecadde
Carlos <digitaldreami@gmail.com>
2013-05-25 20:12:58 -0400

    getter y setter clase usuario

--- prospectCRMEJB/ejbModule/cl/prospect/crm/to/PermisoTo.java
+++ prospectCRMEJB/ejbModule/cl/prospect/crm/to/PermisoTo.java
@@ -3,4 +3,21 @@ package cl.prospect.crm.to;
 public class PermisoTo {
 	private long id;
 	private boolean agregarProspecto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public boolean isAgregarProspecto() {
+		return agregarProspecto;
+	}
+
+	public void setAgregarProspecto(boolean agregarProspecto) {
+		this.agregarProspecto = agregarProspecto;
+	}
+
 }
--- prospectCRMEJB/ejbModule/cl/prospect/crm/to/UsuarioTo.java
+++ prospectCRMEJB/ejbModule/cl/prospect/crm/to/UsuarioTo.java
@@ -1,4 +1,3 @@
-
 package cl.prospect.crm.to;
 
 public class UsuarioTo {
@@ -9,4 +8,61 @@ public class UsuarioTo {
 	private EstadoTo estado;
 	private PersonaTo persona;
 	private SedeTo sede;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public RolTo getRol() {
+		return rol;
+	}
+
+	public void setRol(RolTo rol) {
+		this.rol = rol;
+	}
+
+	public EstadoTo getEstado() {
+		return estado;
+	}
+
+	public void setEstado(EstadoTo estado) {
+		this.estado = estado;
+	}
+
+	public PersonaTo getPersona() {
+		return persona;
+	}
+
+	public void setPersona(PersonaTo persona) {
+		this.persona = persona;
+	}
+
+	public SedeTo getSede() {
+		return sede;
+	}
+
+	public void setSede(SedeTo sede) {
+		this.sede = sede;
+	}
+
 }
