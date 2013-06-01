c724046fc1034e550f50591fc492b8bb5d59a10b
Luis González <lgaedo@gmail.com>
2013-06-01 18:10:45 -0400

    IPermisoDao

---
+++ prospectCRMEJB/ejbModule/cl/prospect/crm/dao/interfaces/IPermisoDao.java
@@ -0,0 +1,15 @@
+package cl.prospect.crm.dao.interfaces;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import cl.prospect.crm.to.PermisoTo;
+
+public interface IPermisoDao {
+
+	public PermisoTo getById(Long id) throws SQLException;
+	public List<PermisoTo> getAll() throws SQLException;
+	public List<PermisoTo> getByParameters(String parameter,String value) throws SQLException;
+	public Long save(PermisoTo p) throws SQLException;
+	
+}
