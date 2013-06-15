b1853a29679534365aada267823f8f4d02a5e353
Luis González <lgaedo@gmail.com>
2013-06-14 20:02:11 -0400

    DireccionDao.java

---
+++ prospectCRMEJB/ejbModule/cl/prospect/crm/dao/DireccionDao.java
@@ -0,0 +1,36 @@
+package cl.prospect.crm.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import cl.prospect.crm.dao.interfaces.IDireccionDao;
+import cl.prospect.crm.to.DireccionTo;
+
+public class DireccionDao implements IDireccionDao {
+
+	@Override
+	public DireccionTo getById(Long id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DireccionTo> getAll() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DireccionTo> getByParameters(String parameter, String value)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Long save(DireccionTo direccion) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
