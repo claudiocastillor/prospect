daa35bac06739d97ba38e3f7ceb9ab8321dcfa5b
Luis González <lgaedo@gmail.com>
2013-06-14 22:09:49 -0400

    ProspectoDaoImpl.java

--- prospectCRMEJB/ejbModule/cl/prospect/crm/dao/impl/ProspectoDaoImpl.java
+++ prospectCRMEJB/ejbModule/cl/prospect/crm/dao/impl/ProspectoDaoImpl.java
@@ -3,12 +3,14 @@ package cl.prospect.crm.dao.impl;
 import java.sql.SQLException;
 import java.util.List;
 
+import cl.prospect.crm.dao.ProspectoDao;
 import cl.prospect.crm.dao.interfaces.IProspectoDao;
 import cl.prospect.crm.to.ProspectoTo;
 
 public class ProspectoDaoImpl implements IProspectoDao{
 
-	IProspectoDao iprospectoDao;
+	IProspectoDao iprospectoDao = new ProspectoDao();
+	
 	
 	@Override
 	public ProspectoTo getById(Long id) throws SQLException {
